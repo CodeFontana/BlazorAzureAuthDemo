7ae6f9c441309ed7f589db832567f816aab230e0
Brian Fontana <brian@codefoxtrot.com>
2024-03-05 22:10:23 -0500

    Add Azure OIDC authentication

---
+++ BlazorUI/LoginLogoutEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorUI;
+
+internal static class LoginLogoutEndpointRouteBuilderExtensions
+{
+    internal static IEndpointConventionBuilder MapLoginAndLogout(this IEndpointRouteBuilder endpoints)
+    {
+        RouteGroupBuilder group = endpoints.MapGroup("");
+
+        group.MapGet("/login", (string? returnUrl) => TypedResults.Challenge(GetAuthProperties(returnUrl)))
+            .AllowAnonymous();
+
+        group.MapPost("/logout", ([FromForm] string? returnUrl) => TypedResults.SignOut(GetAuthProperties(returnUrl),
+            [CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme]));
+
+        return group;
+    }
+
+    private static AuthenticationProperties GetAuthProperties(string? returnUrl)
+    {
+        const string PathBase = "/";
+
+        if (string.IsNullOrEmpty(returnUrl))
+        {
+            returnUrl = PathBase;
+        }
+        else if (Uri.IsWellFormedUriString(returnUrl, UriKind.Relative) == false)
+        {
+            returnUrl = new Uri(returnUrl, UriKind.Absolute).PathAndQuery;
+        }
+        else if (returnUrl[0] != '/')
+        {
+            returnUrl = $"{PathBase}{returnUrl}";
+        }
+
+        return new AuthenticationProperties { RedirectUri = returnUrl };
+    }
+}
--- BlazorUI/Program.cs
+++ BlazorUI/Program.cs
@@ -1,13 +1,25 @@
+using BlazorUI;
 using BlazorUI.Components;
 using BlazorUI.Interfaces;
 using BlazorUI.Services;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<ICookieService, CookieService>();
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(options =>
+    {
+        builder.Configuration.Bind("AzureAd", options);
+        options.TokenValidationParameters.ValidateIssuer = false;
+    });
+builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment() == false)
@@ -19,6 +31,7 @@ if (app.Environment.IsDevelopment() == false)
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapControllers();
+app.MapGroup("/authentication").MapLoginAndLogout();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 app.Run();
